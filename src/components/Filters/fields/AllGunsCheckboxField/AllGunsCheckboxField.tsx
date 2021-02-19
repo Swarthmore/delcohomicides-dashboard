bb2de7be22fbad75cb976e8b081debad4ae63bcd
noot <devnoot@gmail.com>
2021-02-19 10:52:20 -0500

    re-add select all weps func

--- src/components/Filters/fields/AllGunsCheckboxField/AllGunsCheckboxField.tsx
+++ src/components/Filters/fields/AllGunsCheckboxField/AllGunsCheckboxField.tsx
@@ -1,10 +1,38 @@
 import * as React from "react";
 import CheckboxField from "../../common/CheckboxField/CheckboxField";
 import { FiltersContext, Filters } from "../../../../contexts/Filters";
+import { DGVField } from "../../../../types";
 
 export default function AllGunsCheckboxField() {
 
     const ctx: Filters = React.useContext(FiltersContext);
+
+    
+    const allGunsList: DGVField.WeaponType[] = ["Firearm", "Handgun", "Other Gun", "Rifle", "Shotgun"]; 
+
+    // pass in the current form value for weapon types and ADD all guns to it
+    // duplicates will not be returned
+    function selectAllGuns(arr: DGVField.WeaponType[]): DGVField.WeaponType[] {
+        // This is the list of weapon types that will be enabled when the useAllGuns filter is checked
+        return Array.from(new Set([...arr, ...allGunsList]))
+    }
+    
+    // pass in the current form value for weapon types and REMOVE all guns to it
+    // duplicates will not be returned
+    function removeAllGuns(arr: DGVField.WeaponType[]): DGVField.WeaponType[] {
+        return arr.filter(item => !allGunsList.includes(item)).filter(i => i === i)
+    }
+
+    // when all guns checkbox is changed, set the fields accordingly
+    React.useEffect(() => {
+
+        ctx.values.allGunsCbox ? (
+            ctx.setters.weaponTypes(selectAllGuns(ctx.values.weaponTypes))
+        ) : (
+            ctx.setters.weaponTypes(removeAllGuns(ctx.values.weaponTypes))
+        )
+
+    }, [ctx.values.allGunsCbox])
 
     return (
         <CheckboxField
